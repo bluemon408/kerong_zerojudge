505b2a82ae89f1ec8dd310ac8ea322e65dbe90d0
kerong <70834651+kerong2002@users.noreply.github.com>
2022-03-15 17:59:45 +0800

    Add files via upload

--- a007 判斷質數.c
+++ a007 判斷質數.c
@@ -1,53 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#define MAX 47000
-
-int primeList[5000], pLen, n;
-char isPrime;
-
-int cmp(const void* lhs, const void* rhs)
-{
-    return (*(int*)lhs - *(int*)rhs);
-}
-
-void SetPrimeList()
-{
-    char isNot[MAX] = { 0 };
-    primeList[pLen++] = 2;
-    for (int i = 3; i < MAX; i += 2)
-    {
-        if (isNot[i])continue;
-        primeList[pLen++] = i;
-        for (int j = i << 1; j < MAX; j += i)
-            isNot[j] = 1;
-    }
-}
-
-int main()
-{
-    SetPrimeList();
-    while (scanf(" %d", &n) == 1)
-    {
-        isPrime = 0;
-        if (n < MAX)
-        {
-            int* item = (int*)bsearch(&n, primeList, pLen, sizeof(int), cmp);
-            isPrime = (item != NULL);
-        }
-        else
-        {
-            isPrime = 1;
-            for (int i = 0, max = sqrt(n); primeList[i] <= max; i++)
-            {
-                if (!(n % primeList[i]))
-                {
-                    isPrime = 0;
-                    break;
-                }
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<limits.h>
+#include<math.h>
+int max=1;
+int prime[5000];
+void pick(){
+    prime[0]=2;
+    for(int x=3;x<sqrt(INT_MAX);x++){
+        int found=0;
+        for(int y=0;y<max;y++){
+            if(x%prime[y]==0){
+                found=1;
+                break;
+            }
+            if(found!=1 && y==max-1){
+                prime[max]=x;
+                max++;
             }
         }
-        printf("%s\n",(isPrime)?"質數":"非質數");
+    }
+}
+int main(){
+    pick();
+    int n;
+    while(scanf("%d",&n)!=EOF){
+        for(int x=0;x<max;x++){
+            if(n==1){
+                printf("非質數\n");
+                break;
+            }
+            if(n==prime[x]){
+                printf("質數\n");
+                break;
+            }
+            if(n%prime[x]==0){
+                printf("非質數\n");
+                break;
+            }
+            else if(x==max-1){
+                printf("質數\n");
+            }
+        }
     }
     return 0;
 }
