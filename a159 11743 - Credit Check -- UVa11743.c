8d95d7aaeef8e4c3de1c1aff374ea11c7a4125a2
kerong <70834651+kerong2002@users.noreply.github.com>
2022-07-31 15:22:27 +0800

    Add files via upload

--- a159 11743 - Credit Check -- UVa11743.c
+++ a159 11743 - Credit Check -- UVa11743.c
@@ -5,37 +5,24 @@ int main(){
     scanf("%d%*c",&n);
     while(n--){
         char list[30]={'\0'};
-        char data[4][10]={'\0'};
         scanf("%[^\n]%*c",list);
-        char *d=" ";
-        char *pick = strtok(list, d);
-        int put=0;
-        while (pick != NULL) {
-            strcpy(data[put++],pick);
-            pick = strtok(NULL, d);
-        }
-        int even_list[8]={0};
+        int sum=0;
         int cnt=0;
-        for(int y=0;y<4;y++){
-            for(int x=0;x<4;x+=2){
-                even_list[cnt]=2*(data[y][x]-'0');
-                cnt+=1;
+        for(int x=0;x<strlen(list);x++){
+            if(list[x]==32) continue;
+            if(cnt%2==0){
+                int pick=2*(list[x]-'0');
+                while(pick>0){
+                    sum+=pick%10;
+                    pick/=10;
+                }
             }
-        }
-        int even=0;
-        for(int y=0;y<8;y++){
-            while(even_list[y]>0){
-                even+=even_list[y]%10;
-                even_list[y]/=10;
+            else{
+                sum+=list[x]-'0';
             }
+            cnt+=1;
         }
-        int odd=0;
-        for(int y=0;y<4;y++){
-            for(int x=1;x<4;x+=2){
-                odd+=data[y][x]-'0';
-            }
-        }
-        printf("%s\n",((odd+even)%10==0)?"Valid":"Invalid");
+        printf("%s\n",((sum)%10==0)?"Valid":"Invalid");
     }
     return 0;
 }
